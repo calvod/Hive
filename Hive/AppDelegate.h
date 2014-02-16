deab247787a79d3b66394e6c5eb8f20ac54fe093
calvod <calvod@umich.edu>
2014-02-16 18:02:25 -0500

    Initial Commit

---
+++ Hive/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Hive
+//
+//  Created by Diego Calvo on 2/16/14.
+//  Copyright (c) 2014 Diego Calvo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
